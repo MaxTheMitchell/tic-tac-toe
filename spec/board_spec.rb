cd8d39a29a4f6e92a79241681a9124922a85a91e
MaxTheMitchell <rat2043@gmail.com>
2020-06-09 09:35:40 -0700

    add locations tests to board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,23 @@
-require_relative '../lib/board'
+require_relative "../lib/board"
 
-describe 'A Tic Tac Toe board' do
+describe "A Tic Tac Toe board" do
+  let(:board) { Board.new }
 
-  it 'exists' do
+  it "exists" do
     Board.new
   end
+
+  it "has locations" do
+    expect(board).to respond_to(:locations)
+  end
+
+  it "can be given a locations value" do
+    expect(Board.new("FAKE").locations).to eq("FAKE")
+  end
+
+  # it 'can place tokens'
+  #   board.place(:fake,1,1)
+  #   board.locations
+  # end
 
 end
