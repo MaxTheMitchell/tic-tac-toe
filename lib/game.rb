85bb4f126936ab5759bdf383922f97e9951b97c2
MaxTheMitchell <rat2043@gmail.com>
2020-06-09 09:13:14 -0700

    pass tie? test in game spec

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,8 @@
 require_relative "board"
 
 class Game
-
   attr_reader :board
-  
+
   def initialize(board = Board.new)
     @board = board
   end
@@ -12,4 +11,11 @@ class Game
     board.full?
   end
 
+  def tie?
+    board.full? and winner.nil?
+  end
+
+  def winner
+    nil
+  end
 end
